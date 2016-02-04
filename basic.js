03c20b079229a6a0b4d7aaf96ca4393be5db0d04
Wyatt Preul <wpreul@gmail.com>
2016-02-04 13:07:01 -0600

    Linting

--- basic.js
+++ basic.js
@@ -1,40 +1,39 @@
 /* Copyright (c) 2011-2015 Richard Rodger */
 'use strict'
 
-var path = require('path')
-
+var Path = require('path')
 var Note = require('./lib/note')
 var Entity = require('./lib/entity')
-var uniqueid = require('./lib/uniqueid')
-var common = require('./lib/common')
+var Uniqueid = require('./lib/uniqueid')
+var Common = require('./lib/common')
 
 function registerNote (seneca, note) {
   // The note patterns let you pass information to plugins that are
   // loaded after the current plugin. See seneca-admin
-  seneca.add({role: common.plugin.name, note: true, cmd: 'set'}, note.set)
-  seneca.add({role: common.plugin.name, note: true, cmd: 'get'}, note.get)
-  seneca.add({role: common.plugin.name, note: true, cmd: 'list'}, note.list)
-  seneca.add({role: common.plugin.name, note: true, cmd: 'push'}, note.push)
-  seneca.add({role: common.plugin.name, note: true, cmd: 'pop'}, note.pop)
+  seneca.add({role: Common.plugin.name, note: true, cmd: 'set'}, note.set)
+  seneca.add({role: Common.plugin.name, note: true, cmd: 'get'}, note.get)
+  seneca.add({role: Common.plugin.name, note: true, cmd: 'list'}, note.list)
+  seneca.add({role: Common.plugin.name, note: true, cmd: 'push'}, note.push)
+  seneca.add({role: Common.plugin.name, note: true, cmd: 'pop'}, note.pop)
 
   // Legacy cmds use role:'util'
-  seneca.add({role: 'util', note: true, cmd: 'set', deprecate$: common.messages.UTIL_DEPRECATED}, note.set)
-  seneca.add({role: 'util', note: true, cmd: 'get', deprecate$: common.messages.UTIL_DEPRECATED}, note.get)
-  seneca.add({role: 'util', note: true, cmd: 'list', deprecate$: common.messages.UTIL_DEPRECATED}, note.list)
-  seneca.add({role: 'util', note: true, cmd: 'push', deprecate$: common.messages.UTIL_DEPRECATED}, note.push)
-  seneca.add({role: 'util', note: true, cmd: 'pop', deprecate$: common.messages.UTIL_DEPRECATED}, note.pop)
+  seneca.add({role: 'util', note: true, cmd: 'set', deprecate$: Common.messages.UTIL_DEPRECATED}, note.set)
+  seneca.add({role: 'util', note: true, cmd: 'get', deprecate$: Common.messages.UTIL_DEPRECATED}, note.get)
+  seneca.add({role: 'util', note: true, cmd: 'list', deprecate$: Common.messages.UTIL_DEPRECATED}, note.list)
+  seneca.add({role: 'util', note: true, cmd: 'push', deprecate$: Common.messages.UTIL_DEPRECATED}, note.push)
+  seneca.add({role: 'util', note: true, cmd: 'pop', deprecate$: Common.messages.UTIL_DEPRECATED}, note.pop)
 }
 
 function registerEntity (seneca, entity) {
   // TODO: this should be a utility function, not a pattern
   seneca.add({
-    role: common.plugin.name,
+    role: Common.plugin.name,
     cmd: 'ensure_entity',
     pin: {required$: true},
     // TODO: accept entity spec here, e.g. strings like 'sys/user'
     entmap: {object$: true, required$: true}
   }, entity.ensure)
-  seneca.add({role: common.plugin.name, cmd: 'define_sys_entity'}, entity.defineSys)
+  seneca.add({role: Common.plugin.name, cmd: 'define_sys_entity'}, entity.defineSys)
 
   // Legacy cmds use role:'util'
   seneca.add({role: 'util', cmd: 'ensure_entity'}, entity.ensure)
@@ -42,12 +41,12 @@ function registerEntity (seneca, entity) {
 }
 
 function registerUniqueId (seneca) {
-  seneca.add({role: common.plugin.name, cmd: 'quickcode', deprecate$: common.messages.MARKED_FOR_REMOVAL}, uniqueid.quickcode)
-  seneca.add({role: common.plugin.name, cmd: 'generate_id'}, uniqueid.generate_id)
+  seneca.add({role: Common.plugin.name, cmd: 'quickcode', deprecate$: Common.messages.MARKED_FOR_REMOVAL}, Uniqueid.quickcode)
+  seneca.add({role: Common.plugin.name, cmd: 'generate_id'}, Uniqueid.generate_id)
 
   // Legacy cmds use role:'util'
-  seneca.add({role: 'util', cmd: 'quickcode', deprecate$: common.messages.UTIL_DEPRECATED}, uniqueid.quickcode)
-  seneca.add({role: 'util', cmd: 'generate_id'}, uniqueid.generate_id)
+  seneca.add({role: 'util', cmd: 'quickcode', deprecate$: Common.messages.UTIL_DEPRECATED}, Uniqueid.quickcode)
+  seneca.add({role: 'util', cmd: 'generate_id'}, Uniqueid.generate_id)
 }
 
 module.exports = function basic (options) {
@@ -64,7 +63,7 @@ module.exports = function basic (options) {
 
   var utilfuncs = {
     pathnorm: function (pathstr) {
-      return path.normalize((pathstr == null) ? '' : '' + pathstr).replace(/\/+$/, '')
+      return Path.normalize((pathstr == null) ? '' : '' + pathstr).replace(/\/+$/, '')
     },
     deepextend: seneca.util.deepextend
   }
