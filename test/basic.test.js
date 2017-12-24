bd538341db575b4f07ebdea2908625ae94915248
EamonnK <amen_cionna@hotmail.com>
2017-12-24 13:15:09 +0000

    Code quality control updated - .eslintrc.js added/prettier installed

--- test/basic.test.js
+++ test/basic.test.js
@@ -3,24 +3,23 @@
 var Assert = require('assert')
 var Async = require('async')
 var Lab = require('lab')
-var lab = exports.lab = Lab.script()
+var lab = (exports.lab = Lab.script())
 
 require('events').EventEmitter.defaultMaxListeners = Infinity
 
 var seneca
 
-function createSeneca () {
+function createSeneca() {
   return require('seneca')({
     log: 'silent',
     define_plugins: {
       basic: false
     }
-  })
-  .use('../basic.js')
+  }).use('../basic.js')
 }
 
-lab.experiment('seneca.basic', function () {
-  lab.beforeEach(function (done) {
+lab.experiment('seneca.basic', function() {
+  lab.beforeEach(function(done) {
     seneca = createSeneca()
     if (seneca.version >= '2.0.0') {
       seneca.use('entity')
@@ -28,10 +27,15 @@ lab.experiment('seneca.basic', function () {
     seneca.ready(done)
   })
 
-  lab.test('note get', function (done) {
-    seneca.act('role:basic,note:true,cmd:set,key:name,value:app1', function (err) {
+  lab.test('note get', function(done) {
+    seneca.act('role:basic,note:true,cmd:set,key:name,value:app1', function(
+      err
+    ) {
       Assert.equal(err, null)
-      seneca.act('role:basic,note:true,cmd:get,key:name', function (err, response) {
+      seneca.act('role:basic,note:true,cmd:get,key:name', function(
+        err,
+        response
+      ) {
         Assert.equal(err, null)
         Assert.ok(response)
         Assert.ok(response.value)
@@ -41,10 +45,15 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-  lab.test('note list', function (done) {
-    seneca.act('role:basic,note:true,cmd:push,key:name,value:app1', function (err) {
+  lab.test('note list', function(done) {
+    seneca.act('role:basic,note:true,cmd:push,key:name,value:app1', function(
+      err
+    ) {
       Assert.equal(err, null)
-      seneca.act('role:basic,note:true,cmd:list,key:name', function (err, response) {
+      seneca.act('role:basic,note:true,cmd:list,key:name', function(
+        err,
+        response
+      ) {
         Assert.equal(err, null)
         Assert.ok(response)
         Assert.deepEqual(response, ['app1'])
@@ -53,8 +62,8 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-  lab.test('note list empty', function (done) {
-    seneca.act('role:basic,note:true,cmd:list,key:name', function (err, value) {
+  lab.test('note list empty', function(done) {
+    seneca.act('role:basic,note:true,cmd:list,key:name', function(err, value) {
       Assert.equal(err, null)
       Assert.ok(value)
       Assert.deepEqual(value, [])
@@ -62,22 +71,32 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-
-  lab.test('note pop', function (done) {
-    seneca.act('role:basic,note:true,cmd:push,key:name,value:app1', function (err) {
+  lab.test('note pop', function(done) {
+    seneca.act('role:basic,note:true,cmd:push,key:name,value:app1', function(
+      err
+    ) {
       Assert.equal(err, null)
-      seneca.act('role:basic,note:true,cmd:list,key:name', function (err, response) {
+      seneca.act('role:basic,note:true,cmd:list,key:name', function(
+        err,
+        response
+      ) {
         Assert.equal(err, null)
         Assert.ok(response)
         Assert.deepEqual(response, ['app1'])
 
-        seneca.act('role:basic,note:true,cmd:pop,key:name', function (err, response) {
+        seneca.act('role:basic,note:true,cmd:pop,key:name', function(
+          err,
+          response
+        ) {
           Assert.equal(err, null)
           Assert.ok(response)
           Assert.ok(response.value)
           Assert.equal(response.value, 'app1')
 
-          seneca.act('role:basic,note:true,cmd:list,key:name', function (err, response) {
+          seneca.act('role:basic,note:true,cmd:list,key:name', function(
+            err,
+            response
+          ) {
             Assert.equal(err, null)
             Assert.ok(response)
             Assert.deepEqual(response, [])
@@ -89,72 +108,101 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-  lab.test('note list and values have different namespaces', function (done) {
-    Async.series([
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:push,key:name,value:1', function (err) {
-          Assert.equal(err, null)
-          next()
-        })
-      },
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:set,key:name,value:2', function (err) {
-          Assert.ok(!err)
-          next()
-        })
-      },
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:get,key:name', function (err, response) {
-          Assert.equal(err, null)
-          Assert.ok(response)
-          Assert.equal(response.value, '2')
-          next()
-        })
-      },
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:list,key:name', function (err, response) {
-          Assert.equal(err, null)
-          Assert.ok(response)
-          Assert.deepEqual(response, ['1'])
-          next()
-        })
-      }], done)
+  lab.test('note list and values have different namespaces', function(done) {
+    Async.series(
+      [
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:push,key:name,value:1', function(
+            err
+          ) {
+            Assert.equal(err, null)
+            next()
+          })
+        },
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:set,key:name,value:2', function(
+            err
+          ) {
+            Assert.ok(!err)
+            next()
+          })
+        },
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:get,key:name', function(
+            err,
+            response
+          ) {
+            Assert.equal(err, null)
+            Assert.ok(response)
+            Assert.equal(response.value, '2')
+            next()
+          })
+        },
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:list,key:name', function(
+            err,
+            response
+          ) {
+            Assert.equal(err, null)
+            Assert.ok(response)
+            Assert.deepEqual(response, ['1'])
+            next()
+          })
+        }
+      ],
+      done
+    )
   })
 
-  lab.test('note push', function (done) {
-    Async.series([
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:push,key:k0,value:i0', function (err) {
-          Assert.equal(err, null)
-          next()
-        })
-      },
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:list,key:k0', function (err, out) {
-          Assert.equal(err, null)
-          Assert.deepEqual(['i0'], out)
-          next()
-        })
-      },
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:push,key:k0,value:i1', function (err, response) {
-          Assert.equal(err, null)
+  lab.test('note push', function(done) {
+    Async.series(
+      [
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:push,key:k0,value:i0', function(
+            err
+          ) {
+            Assert.equal(err, null)
+            next()
+          })
+        },
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:list,key:k0', function(
+            err,
+            out
+          ) {
+            Assert.equal(err, null)
+            Assert.deepEqual(['i0'], out)
+            next()
+          })
+        },
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:push,key:k0,value:i1', function(
+            err,
+            response
+          ) {
+            Assert.equal(err, null)
 
-          next()
-        })
-      },
-      function (next) {
-        seneca.act('role:basic,note:true,cmd:list,key:k0', function (err, out) {
-          Assert.equal(err, null)
-          Assert.deepEqual(['i0', 'i1'], out)
+            next()
+          })
+        },
+        function(next) {
+          seneca.act('role:basic,note:true,cmd:list,key:k0', function(
+            err,
+            out
+          ) {
+            Assert.equal(err, null)
+            Assert.deepEqual(['i0', 'i1'], out)
 
-          next()
-        })
-      }], done)
+            next()
+          })
+        }
+      ],
+      done
+    )
   })
 
-  lab.test('quickcode basic', function (done) {
-    seneca.act({role: 'basic', cmd: 'quickcode'}, function (err, code) {
+  lab.test('quickcode basic', function(done) {
+    seneca.act({ role: 'basic', cmd: 'quickcode' }, function(err, code) {
       Assert.ifError(err)
       Assert.ok(code)
       Assert.equal(code.length, 8)
@@ -163,8 +211,11 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-  lab.test('quickcode basic with length', function (done) {
-    seneca.act({role: 'basic', cmd: 'quickcode', length: 12}, function (err, code) {
+  lab.test('quickcode basic with length', function(done) {
+    seneca.act({ role: 'basic', cmd: 'quickcode', length: 12 }, function(
+      err,
+      code
+    ) {
       Assert.ifError(err)
       Assert.ok(code)
       Assert.equal(code.length, 12)
@@ -173,8 +224,24 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-  lab.test('quickcode basic with alphabet', function (done) {
-    seneca.act({role: 'basic', cmd: 'quickcode', alphabet: 'abcdef'}, function (err, code) {
+  lab.test('quickcode basic with alphabet', function(done) {
+    seneca.act(
+      { role: 'basic', cmd: 'quickcode', alphabet: 'abcdef' },
+      function(err, code) {
+        Assert.ifError(err)
+        Assert.ok(code)
+        Assert.equal(code.length, 8)
+        Assert.ok(/[ABCDEF]/.exec(code) == null)
+        done()
+      }
+    )
+  })
+
+  lab.test('quickcode basic with curses', function(done) {
+    seneca.act({ role: 'basic', cmd: 'quickcode', curses: 'damn' }, function(
+      err,
+      code
+    ) {
       Assert.ifError(err)
       Assert.ok(code)
       Assert.equal(code.length, 8)
@@ -183,18 +250,8 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-  lab.test('quickcode basic with curses', function (done) {
-    seneca.act({role: 'basic', cmd: 'quickcode', curses: 'damn'}, function (err, code) {
-      Assert.ifError(err)
-      Assert.ok(code)
-      Assert.equal(code.length, 8)
-      Assert.ok(/[ABCDEF]/.exec(code) == null)
-      done()
-    })
-  })
-
-  lab.test('generate_id basic', function (done) {
-    seneca.act({ role: 'basic', cmd: 'generate_id' }, function (err, id) {
+  lab.test('generate_id basic', function(done) {
+    seneca.act({ role: 'basic', cmd: 'generate_id' }, function(err, id) {
       Assert.ifError(err)
       Assert.ok(id)
       Assert.equal(id.length, 6)
@@ -202,48 +259,63 @@ lab.experiment('seneca.basic', function () {
     })
   })
 
-  lab.test('define_sys_entity', function (done) {
-    seneca.act({ role: 'basic', cmd: 'define_sys_entity' }, function (err, resp) {
+  lab.test('define_sys_entity', function(done) {
+    seneca.act({ role: 'basic', cmd: 'define_sys_entity' }, function(
+      err,
+      resp
+    ) {
       Assert.ifError(err)
       Assert.ok(resp)
       done()
     })
   })
 
-  lab.test('define_sys_entity with list', function (done) {
-    seneca.act({ role: 'basic', cmd: 'define_sys_entity', list: 'entity' }, function (err, resp) {
+  lab.test('define_sys_entity with list', function(done) {
+    seneca.act(
+      { role: 'basic', cmd: 'define_sys_entity', list: 'entity' },
+      function(err, resp) {
+        Assert.ifError(err)
+        Assert.ok(resp)
+        done()
+      }
+    )
+  })
+
+  lab.test('define_sys_entity with list and string entity', function(done) {
+    seneca.act(
+      { role: 'basic', cmd: 'define_sys_entity', list: [{ entity: 'ent' }] },
+      function(err, resp) {
+        Assert.ifError(err)
+        Assert.ok(resp)
+        done()
+      }
+    )
+  })
+
+  lab.test('define_sys_entity with list and non object entity', function(done) {
+    seneca.act({ role: 'basic', cmd: 'define_sys_entity', list: [1] }, function(
+      err,
+      resp
+    ) {
       Assert.ifError(err)
       Assert.ok(resp)
       done()
     })
   })
 
-  lab.test('define_sys_entity with list and string entity', function (done) {
-    seneca.act({ role: 'basic', cmd: 'define_sys_entity', list: [{entity: 'ent'}] }, function (err, resp) {
-      Assert.ifError(err)
-      Assert.ok(resp)
-      done()
-    })
-  })
-
-  lab.test('define_sys_entity with list and non object entity', function (done) {
-    seneca.act({ role: 'basic', cmd: 'define_sys_entity', list: [1] }, function (err, resp) {
-      Assert.ifError(err)
-      Assert.ok(resp)
-      done()
-    })
-  })
-
-  lab.test('define_sys_entity with list and a seneca entity', function (done) {
+  lab.test('define_sys_entity with list and a seneca entity', function(done) {
     var product = seneca.make('product')
-    seneca.act({ role: 'basic', cmd: 'define_sys_entity', list: [product] }, function (err, resp) {
-      Assert.ifError(err)
-      Assert.ok(resp)
-      done()
-    })
+    seneca.act(
+      { role: 'basic', cmd: 'define_sys_entity', list: [product] },
+      function(err, resp) {
+        Assert.ifError(err)
+        Assert.ok(resp)
+        done()
+      }
+    )
   })
 
-  lab.test('utilfuncs pathnorm empty path', function (done) {
+  lab.test('utilfuncs pathnorm empty path', function(done) {
     var basic = createSeneca().export('basic')
     var path = basic.pathnorm('')
     Assert.ok(path)
@@ -251,7 +323,7 @@ lab.experiment('seneca.basic', function () {
     done()
   })
 
-  lab.test('utilfuncs pathnorm null path', function (done) {
+  lab.test('utilfuncs pathnorm null path', function(done) {
     var basic = createSeneca().export('basic')
     var path = basic.pathnorm(null)
     Assert.ok(path)
